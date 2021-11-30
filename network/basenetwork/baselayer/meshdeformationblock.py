e22d50ebc90a9118cb6ede0b63b9016ca9a6f6f9
Marco Mameli <mameli.1.marco@gmail.com>
2021-11-30 15:31:05 +0100

    Updated the mesh deformation block
    Added the vertex features for the forward

--- network/basenetwork/baselayer/meshdeformationblock.py
+++ network/basenetwork/baselayer/meshdeformationblock.py
@@ -6,7 +6,6 @@
 from torch import nn
 from pytorch3d.ops import vert_align, GraphConv
 
-
 class MeshDeformationBlock(nn.Module):
     def __init__(self, graph_input_dim: int, graph_output_dim: int) -> None:
         """Init of the layer
@@ -19,7 +18,7 @@ class MeshDeformationBlock(nn.Module):
         self.graph_conv1 = GraphConv(input_dim=graph_input_dim, output_dim=graph_output_dim)
         self.graph_conv2 = GraphConv(input_dim=graph_input_dim, output_dim=graph_output_dim)
         self.graph_conv3 = GraphConv(input_dim=graph_input_dim, output_dim=graph_output_dim)
-    def forward(self, img_features, vertex_position):
+    def forward(self, img_features, vertex_position, vertex_padded):
         """Forward pass for the Mesh Deformation Block
 
         :param img_features: the images features extracted from an image (using CNN)
@@ -30,10 +29,11 @@ class MeshDeformationBlock(nn.Module):
         :rtype: torch.tensor
         """
         pix2vert_features = vert_align(img_features, verts=vertex_position)
-        graph_input = pix2vert_features + vertex_position
+        graph_input = pix2vert_features + vertex_padded
         mesh_deformation = self.graph_conv1(graph_input)
         mesh_deformation1 = self.graph_conv2(mesh_deformation)
         mesh_deformation1 = self.graph_conv3(mesh_deformation1)
         mesh_deformation_output = mesh_deformation + mesh_deformation1
         return mesh_deformation_output
-        
+
+ 
