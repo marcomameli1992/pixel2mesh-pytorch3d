9ae131f7b9ad97a71f9c7fcc9604026f0b64270e
Marco Mameli <mameli.1.marco@gmail.com>
2021-11-30 14:09:17 +0100

    Solved some dimensionality problems
    This version solve the dimension of the convolutional layer

--- network/basenetwork/convolutionalnetwork.py
+++ network/basenetwork/convolutionalnetwork.py
@@ -12,88 +12,89 @@ class ConvolutionalNetwork(nn.Module):
         
         """
         super(ConvolutionalNetwork, self).__init__()
-        ks = (3,3)
+        conv_kernel_size = (3,3)
+        pooling_kernel_size = (2,2)
         self.pooling = pooling
         
         #%224x224
-        self.conv1_1 = nn.Conv2d(in_channels=input_channel, out_channels=16, kernel_size=ks, stride=1)
-        self.conv1_2 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=ks, stride=1)
+        self.conv1_1 = nn.Conv2d(in_channels=input_channel, out_channels=16, kernel_size=conv_kernel_size, stride=1)
+        self.conv1_2 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=conv_kernel_size, stride=1, padding=1)
         if self.pooling:
             if pooling_type not in ["Max", "Avg"]:
                 raise ValueError("Pooling type has not one of the value 'Max' or 'Avg'")
-            elif pooling_type is "Max":
-                self.conv1_3 = nn.MaxPool2d(kernel_size=ks, stride=1)
-            elif pooling_type is "Avg":
-                self.conv1_3 = nn.AvgPool2d(kernel_size=ks, stride=1)
+            elif pooling_type == "Max":
+                self.conv1_3 = nn.MaxPool2d(kernel_size=pooling_kernel_size, stride=2, padding=1)
+            elif pooling_type == "Avg":
+                self.conv1_3 = nn.AvgPool2d(kernel_size=pooling_kernel_size, stride=2, padding=1)
         else:
-            self.conv1_3 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=ks, stride=2)
+            self.conv1_3 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=pooling_kernel_size, stride=2, padding=1)
         #%112x112
         if self.pooling:
-            self.conv2_1 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=ks, stride=1)
+            self.conv2_1 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=conv_kernel_size, stride=1)
         else:
-            self.conv2_1 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=ks, stride=1)
-        self.conv2_2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=ks, stride=1)
+            self.conv2_1 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=conv_kernel_size, stride=1)
+        self.conv2_2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=conv_kernel_size, stride=1, padding=1)
         if self.pooling:
             if pooling_type not in ["Max", "Avg"]:
                 raise ValueError("Pooling type has not one of the value 'Max' or 'Avg'")
-            elif pooling_type is "Max":
-                self.conv2_3 = nn.MaxPool2d(kernel_size=ks, stride=1)
-            elif pooling_type is "Avg":
-                self.conv2_3 = nn.AvgPool2d(kernel_size=ks, stride=1)
+            elif pooling_type == "Max":
+                self.conv2_3 = nn.MaxPool2d(kernel_size=pooling_kernel_size, stride=2, padding=1)
+            elif pooling_type == "Avg":
+                self.conv2_3 = nn.AvgPool2d(kernel_size=pooling_kernel_size, stride=2, padding=1)
         else:
-            self.conv2_3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=ks, stride=2)
+            self.conv2_3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=pooling_kernel_size, stride=2, padding=1)
         #%56x56
         if self.pooling:
-            self.conv3_1 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=ks, stride=1)
+            self.conv3_1 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=conv_kernel_size, stride=1)
         else:
-            self.conv3_1 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=ks, stride=1)
-        self.conv3_2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=ks, stride=1)
+            self.conv3_1 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=conv_kernel_size, stride=1)
+        self.conv3_2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=conv_kernel_size, stride=1, padding=1)
         if self.pooling:
             if pooling_type not in ["Max", "Avg"]:
                 raise ValueError("Pooling type has not one of the value 'Max' or 'Avg'")
-            elif pooling_type is "Max":
-                self.conv3_3 = nn.MaxPool2d(kernel_size=ks, stride=1)
-            elif pooling_type is "Avg":
-                self.conv3_3 = nn.AvgPool2d(kernel_size=ks, stride=1)
+            elif pooling_type == "Max":
+                self.conv3_3 = nn.MaxPool2d(kernel_size=pooling_kernel_size, stride=2, padding=1)
+            elif pooling_type == "Avg":
+                self.conv3_3 = nn.AvgPool2d(kernel_size=pooling_kernel_size, stride=2, padding=1)
         else:
-            self.conv3_3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=ks, stride=2)
+            self.conv3_3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=pooling_kernel_size, stride=2, padding=1)
         #%28x28
         if self.pooling:
-            self.conv4_1 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=ks, stride=1)
+            self.conv4_1 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=conv_kernel_size, stride=1)
         else:
-            self.conv4_1 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=ks, stride=1)
-        self.conv4_2 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=ks, stride=1)
+            self.conv4_1 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=conv_kernel_size, stride=1)
+        self.conv4_2 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=conv_kernel_size, stride=1, padding=1)
         if self.pooling:
             if pooling_type not in ["Max", "Avg"]:
                 raise ValueError("Pooling type has not one of the value 'Max' or 'Avg'")
-            elif pooling_type is "Max":
-                self.conv4_3 = nn.MaxPool2d(kernel_size=ks, stride=1)
-            elif pooling_type is "Avg":
-                self.conv4_3 = nn.AvgPool2d(kernel_size=ks, stride=1)
+            elif pooling_type == "Max":
+                self.conv4_3 = nn.MaxPool2d(kernel_size=pooling_kernel_size, stride=2, padding=1)
+            elif pooling_type == "Avg":
+                self.conv4_3 = nn.AvgPool2d(kernel_size=pooling_kernel_size, stride=2, padding=1)
         else:
-            self.conv4_3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=ks, stride=2)
+            self.conv4_3 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=pooling_kernel_size, stride=2, padding=1)
         #%14x14
         if self.pooling:
-            self.conv5_1 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=ks, stride=1)
+            self.conv5_1 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=conv_kernel_size, stride=1)
         else:
-            self.conv5_1 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=ks, stride=1)
-        self.conv5_2 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=ks, stride=1)
+            self.conv5_1 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=conv_kernel_size, stride=1)
+        self.conv5_2 = nn.Conv2d(in_channels=256, out_channels=256, kernel_size=conv_kernel_size, stride=1, padding=1)
         if self.pooling:
             if pooling_type not in ["Max", "Avg"]:
                 raise ValueError("Pooling type has not one of the value 'Max' or 'Avg'")
-            elif pooling_type is "Max":
-                self.conv5_3 = nn.MaxPool2d(kernel_size=ks, stride=1)
-            elif pooling_type is "Avg":
-                self.conv5_3 = nn.AvgPool2d(kernel_size=ks, stride=1)
+            elif pooling_type == "Max":
+                self.conv5_3 = nn.MaxPool2d(kernel_size=pooling_kernel_size, stride=2, padding=1)
+            elif pooling_type == "Avg":
+                self.conv5_3 = nn.AvgPool2d(kernel_size=pooling_kernel_size, stride=2, padding=1)
         else:
-            self.conv5_3 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=ks, stride=2)
+            self.conv5_3 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=pooling_kernel_size, stride=2, padding=1)
         #%7x7
         if self.pooling:
-            self.conv6_1 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=ks, stride=1)
+            self.conv6_1 = nn.Conv2d(in_channels=256, out_channels=512, kernel_size=conv_kernel_size, stride=1)
         else:
-            self.conv6_1 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=ks, stride=1)
-        self.conv6_2 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=ks, stride=1)
-        self.conv6_3 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=ks, stride=1)
+            self.conv6_1 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=conv_kernel_size, stride=1)
+        self.conv6_2 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=conv_kernel_size, stride=1, padding=1)
+        self.conv6_3 = nn.Conv2d(in_channels=512, out_channels=512, kernel_size=conv_kernel_size, stride=1)
         
         
         
@@ -107,8 +108,7 @@ class ConvolutionalNetwork(nn.Module):
         """
         #256x256
         x = ReLU(self.conv1_1(input_img))
-        x = self.conv1_2(x)
-        x = ReLU(x)
+        x = ReLU(self.conv1_2(x))
         conv1_out = x.clone().detach()
         #128x128
         if self.pooling:
@@ -153,3 +153,18 @@ class ConvolutionalNetwork(nn.Module):
         conv6_out = x.clone().detach()
         
         return x, conv1_out, conv2_out, conv3_out, conv4_out, conv5_out, conv6_out
+
+if __name__ == "__main__":
+    """Testing the class
+    """
+    import torch
+    convolutional_model = ConvolutionalNetwork(input_channel=3, pooling=True, pooling_type="Avg")
+    t_input = torch.rand((1,3,224,224), dtype=torch.float32)
+    out, conv1, conv2, conv3, conv4, conv5, conv6 = convolutional_model(t_input)
+    print(f"out shape: {out.shape}")
+    print(f"conv1 shape: {conv1.shape}")
+    print(f"conv2 shape: {conv2.shape}")
+    print(f"conv3 shape: {conv3.shape}")
+    print(f"conv4 shape: {conv4.shape}")
+    print(f"conv5 shape: {conv5.shape}")
+    print(f"conv6 shape: {conv6.shape}")
