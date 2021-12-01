f39f611f41ee72fcb71d65de49f18b5ffe356d8e
Marco Mameli <mameli.1.marco@gmail.com>
2021-12-01 15:55:54 +0100

    Defined the starting point for the forward function
    In this version all the graph block are used in singular configuration on the input mesh.
    
    Next step:
    TODO1 opening the basic mesh and insert the data in the graphconvolution as startmesh
    TODO2 testing the network in this configuration
    TODO3 update the forward function with respect to the pixel2mesh original configuration

--- network/basenetwork/graphnetwork.py
+++ network/basenetwork/graphnetwork.py
@@ -5,13 +5,16 @@
 :rtype: torch.nn.Module
 """
 from torch import nn
-from torch.nn import ModuleDict, ModuleList
+from torch.nn import ModuleList
 from pytorch3d.ops import vert_align, GraphConv
 from pytorch3d.ops.subdivide_meshes import SubdivideMeshes
 
 class GraphConvolution(nn.Module):
     def __init__(self, input_dim: int, hidden_dim: int , output_dim: int = 3, hidden_layer_count: int = 12, start_mesh = None) -> None:
         super(GraphConvolution, self).__init__()
+        
+        #TODO Extracting the vertex from the starting mesh 
+        self.vertices = None 
         
         # Create the First GraphConvolutionBlock
         self.gc1_1 = GraphConv(input_dim=input_dim, output_dim=hidden_dim)
@@ -24,7 +27,7 @@ class GraphConvolution(nn.Module):
         self.gp1 = SubdivideMeshes(meshes=None)
         
         # Create the Second GraphConvolutionBlock
-        self.gc2_1 = GraphConv(input_dim=input_dim, output_dim=hidden_dim)
+        self.gc2_1 = GraphConv(input_dim=input_dim + hidden_dim, output_dim=hidden_dim)
         self.gc_module_2 = ModuleList(
             [GraphConv(input_dim=hidden_dim, output_dim=hidden_dim) for n in range(hidden_layer_count)]
         )
@@ -34,12 +37,39 @@ class GraphConvolution(nn.Module):
         self.gp2 = SubdivideMeshes(meshes=None)
         
         # Create the Third GraphConvolutionBlock
-        self.gc3_1 = GraphConv(input_dim=input_dim, output_dim=hidden_dim)
+        self.gc3_1 = GraphConv(input_dim=input_dim + hidden_dim, output_dim=hidden_dim)
         self.gc_module_3 = ModuleList(
             [GraphConv(input_dim=hidden_dim, output_dim=hidden_dim) for n in range(hidden_layer_count)]
         )
         self.gc3_14 = GraphConv(input_dim=hidden_dim, output_dim=output_dim)
         
         
-    def forward(slef, conv_64, conv_128, conv_256, conv_512):
+    def forward(self, conv64, conv128, conv256, conv512):
+        vert_features_from_conv64 = vert_align(conv64, self.vertices)
         
+        x = self.gc1_1(vert_features_from_conv64)
+        
+        for i, l in enumerate(self.gc_module_1):
+            x = self.gc_module_1[i](x)
+        
+        x = self.gc1_14(x)
+        
+        vert_features_from_conv64 = vert_align(conv128, self.vertices)
+        
+        x = self.gc2_1(vert_features_from_conv64) #TODO here the features are to be concatenated
+        
+        for i, l in enumerate(self.gc_module_2):
+            x = self.gc_module_2[i](x)
+            
+        x = self.gc2_14(x)
+        
+        vert_features_from_conv64 = vert_align(conv256, self.vertices)
+        
+        x = self.gc3_1(vert_features_from_conv64)
+        
+        for i, l in enumerate(self.gc_module_3):
+            x = self.gc_module_3[i](x)
+        
+        x = self.gc3_14(x)
+        
+        return x
