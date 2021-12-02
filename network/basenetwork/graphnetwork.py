1588db9558710b029a86047f7c2eafb7126d326d
Marco Mameli <mameli.1.marco@gmail.com>
2021-12-02 18:24:22 +0100

    Updates to the graph convolutional
    This updates consist of:
    - Adding activation function ReLU
    - Set fixed dimension for the second braph-conv block for input (128 for the moment)
    - change the input of the third graph convolutional block
    - change the input for the forward: added the mesh input to use the mesh from the output
    - deleted the selection of the first tensor in a batch for the graphconv (NOTE: it not works for the moment)
    
    In the test section:
    - a batch of two meshes was created (it causes a problem on the execution of the graphconv
    - changed the batch size for the random features generated for the test
    - update the network call that now uses the mesh as input at the first position of the parameters.

--- network/basenetwork/graphnetwork.py
+++ network/basenetwork/graphnetwork.py
@@ -6,6 +6,7 @@
 """
 
 from torch import nn
+from torch.nn import ReLU
 from torch._C import Graph
 from torch.nn import ModuleList
 from pytorch3d.ops import vert_align, GraphConv
@@ -18,11 +19,10 @@ class GraphConvolution(nn.Module):
     :param nn: Inherit from nn.Module
     :type nn: nn.Module
     """
-    def __init__(self, input_dim: int, hidden_dim: int , output_dim: int = 3, hidden_layer_count: int = 12, start_mesh: Meshes = None) -> None:
+    def __init__(self, input_dim: int, hidden_dim: int , output_dim: int = 3, hidden_layer_count: int = 12) -> None:
         super(GraphConvolution, self).__init__()
         
-        self.vertices = start_mesh.verts_padded() #TODO insert the mesh vertices here (now None for the creation test.)
-        self.mesh = start_mesh
+        self.activation = ReLU()
         
         self.gc1_1 = GraphConv(input_dim=input_dim, output_dim=hidden_dim) # First GraphConvolutional Block
         
@@ -33,7 +33,7 @@ class GraphConvolution(nn.Module):
         
         self.gp1 = SubdivideMeshes(meshes=None) # Substitute the GraphPooling layer in the pixel2mesh TF implementation [the mesh is none because it is used the output from the gc1_14]
         
-        self.gc2_1 = GraphConv(input_dim=input_dim + hidden_dim, output_dim=hidden_dim) # Second GraphConvolutional Block
+        self.gc2_1 = GraphConv(input_dim=128, output_dim=hidden_dim) # Second GraphConvolutional Block
         self.gc_module_2 = ModuleList(
             [GraphConv(input_dim=hidden_dim, output_dim=hidden_dim) for n in range(hidden_layer_count)]
         )
@@ -41,14 +41,14 @@ class GraphConvolution(nn.Module):
         
         self.gp2 = SubdivideMeshes(meshes=None) # Substitute the GraphPooling layer in the pixel2mesh TF implementation [the mesh is none because it is used the output from the gc2_14]
         
-        self.gc3_1 = GraphConv(input_dim=input_dim + hidden_dim, output_dim=hidden_dim) # Third GraphConvolutional Block
+        self.gc3_1 = GraphConv(input_dim=hidden_dim, output_dim=hidden_dim) # Third GraphConvolutional Block
         self.gc_module_3 = ModuleList(
             [GraphConv(input_dim=hidden_dim, output_dim=hidden_dim) for n in range(hidden_layer_count)]
         )
         self.gc3_14 = GraphConv(input_dim=hidden_dim, output_dim=output_dim)
         
         
-    def forward(self, conv64, conv128, conv256, conv512 = None, device="cuda"):
+    def forward(self, mesh, conv64, conv128, conv256, conv512 = None):
         """The forward function
 
         :param conv64: the features tensor from the conv layer with 64 dim output
@@ -62,64 +62,81 @@ class GraphConvolution(nn.Module):
         :return: the output of the convolution
         :rtype: torch.Tensor (it can be changed in pytorch3d.io.Mesh)
         """
-        vert_features_from_conv64 = vert_align(conv64, self.mesh)
         
-        x = self.gc1_1(vert_features_from_conv64[0], self.mesh.edges_packed())
+        #TODO Check if it is possible to use batches
+        
+        vert_features_from_conv64 = vert_align(conv64, mesh)
+        
+        residual1_1 = x = self.activation(self.gc1_1(vert_features_from_conv64, mesh.edges_packed()))
         
         for i, l in enumerate(self.gc_module_1):
-            x = self.gc_module_1[i](x, self.mesh.edges_packed())
+            x = self.gc_module_1[i](x, mesh.edges_packed())
+            x = self.activation(x)
         
-        x = self.gc1_14(x, self.mesh.edges_packed())
+        x = residual1_1 + x
+        
+        vertices = self.activation(self.gc1_14(x, mesh.edges_packed()))
         
         #TODO check reconstruct the mesh with the new vetices
         
-        self.mesh = Meshes(verts=[x], faces=self.mesh.faces_list())
+        mesh_gc_1 = self.mesh = Meshes(verts=[vertices], faces=mesh.faces_list())
         
-        x = self.gp1(self.mesh)
+        self.mesh, features_gp1 = self.gp1(self.mesh, x)
         
-        vert_features_from_conv64 = vert_align(conv128, self.mesh) #TODO change the self.mesh to the output of the pooling.
+        vert_features_from_conv128 = vert_align(conv128, self.mesh) #TODO change the self.mesh to the output of the pooling.
         
-        x = self.gc2_1(vert_features_from_conv64) #TODO here the features are to be concatenated
+        residual2_1 = x = self.activation(self.gc2_1(vert_features_from_conv128, mesh.edges_packed())) #TODO here the features are to be concatenated
         
         for i, l in enumerate(self.gc_module_2):
-            x = self.gc_module_2[i](x)
+            x = self.activation(self.gc_module_2[i](x, mesh.edges_packed()))
             
-        x = self.gc2_14(x)
+        x = residual2_1 + x
+            
+        vertices = self.activation(self.gc2_14(x, mesh.edges_packed()))
         
-        x = self.gp2(x)
+        mesh_gc_2 = self.mesh = Meshes(verts=[vertices], faces=mesh.faces_list())
         
-        vert_features_from_conv64 = vert_align(conv256, self.mesh) #TODO change the self.mesh to the output of the pooling.
+        self.mesh = self.gp2(self.mesh)
         
-        x = self.gc3_1(vert_features_from_conv64) #TODO here the features are to be concatenated
+        vert_features_from_conv256 = vert_align(conv256, mesh) #TODO change the self.mesh to the output of the pooling.
+        
+        residual3_1 = x = self.activation(self.gc3_1(vert_features_from_conv256[0], mesh.edges_packed())) #TODO here the features are to be concatenated
         
         for i, l in enumerate(self.gc_module_3):
-            x = self.gc_module_3[i](x)
+            x = self.activation(self.gc_module_3[i](x, mesh.edges_packed()))
+            
+        x = residual3_1 + x
         
-        x = self.gc3_14(x)
+        x = self.activation(self.gc3_14(x, mesh.edges_packed()))
         
-        return x
+        mesh_gc_3 = self.mesh = Meshes(verts=[x], faces=mesh.faces_list())
+        
+        return mesh_gc_1, mesh_gc_2, mesh_gc_3
     
 if __name__ == "__main__":
     """Network Testing
     """
     import torch
-    from pytorch3d.io import load_obj, load_objs_as_meshes
     from pytorch3d.structures.meshes import Meshes
     from pytorch3d.utils import ico_sphere
+    from pytorch3d.structures import join_meshes_as_batch
     
-    device = "cuda:0" if torch.cuda.is_available() else "cpu" 
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
     
     ico = ico_sphere(3).to(device=device)
     #print("Verts list", ico.verts_list())
     #print("Faces list", ico.faces_list())
     #print("Verts packed", ico.verts_packed())
     
-    conv64 = torch.rand([1, 64, 54, 54], dtype=torch.float32, device=device)
-    conv128 = torch.rand([1, 128, 26, 26], dtype=torch.float32, device=device)
-    conv256 = torch.rand([1, 256, 12, 12], dtype=torch.float32, device=device)
-    conv512 = torch.rand([1, 512, 3, 3], dtype=torch.float32, device=device)
-    gcn = GraphConvolution(input_dim=64, hidden_dim=256 , output_dim = 3, start_mesh=ico) # Need to understand the input dimension and the internal dimension how work
+    mesh_batch = join_meshes_as_batch([ico, ico])
+    
+    conv64 = torch.rand([2, 64, 54, 54], dtype=torch.float, device=device)
+    conv128 = torch.rand([2, 128, 26, 26], dtype=torch.float, device=device)
+    conv256 = torch.rand([2, 256, 12, 12], dtype=torch.float, device=device)
+    conv512 = torch.rand([2, 512, 3, 3], dtype=torch.float, device=device)
+    
+    gcn = GraphConvolution(input_dim=64, hidden_dim=256 , output_dim = 3) # Need to understand the input dimension and the internal dimension how work
     gcn = gcn.to(device=device) # Move on GPU
     #print(GraphConvolution)
-    output = gcn(conv64, conv128, conv256)
+    output = gcn(mesh_batch, conv64, conv128, conv256)
     
