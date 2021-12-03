5640ae87da7fb91fd966d8c0dce55279620555bf
Marco Mameli <mameli.1.marco@gmail.com>
2021-12-03 11:56:21 +0100

    Update graphconvolutionblock
    Added hidden_dim in the creation that is used for the output_dim of the intermedia GraphConv layer

--- network/basenetwork/baseblock/graph/graphblock.py
+++ network/basenetwork/baseblock/graph/graphblock.py
@@ -11,28 +11,29 @@ from torch.nn import ModuleList
 from pytorch3d.ops import GraphConv
 
 class GraphConvolutionalBlock(nn.Module):
-    def __init__(self, input_dim: int, output_dim: int, hidden_layer_count: int = 12):
+    def __init__(self, input_dim: int, hidden_dim: int, output_dim: int, hidden_layer_count: int = 12):
         super(GraphConvolutionalBlock, self).__init__()
         
         self.activation = ReLU()
         
-        self.gc1 = GraphConv(input_dim=input_dim, output_dim=output_dim)
+        self.gc1 = GraphConv(input_dim=input_dim, output_dim=hidden_dim)
         
         self.gc_module = ModuleList(
-            [GraphConv(input_dim=output_dim, output_dim=output_dim) for n in range(hidden_layer_count)]
+            [GraphConv(input_dim=hidden_dim, output_dim=hidden_dim) for n in range(hidden_layer_count)]
         )
-        self.gc14 = GraphConv(input_dim=output_dim, output_dim=output_dim)
+        self.gc14 = GraphConv(input_dim=hidden_dim, output_dim=output_dim)
     
     def forward(self, features, edges):
         
-        residual1_1 = x = self.activation(self.gc1_1(features, edges))
+        residual = x = self.activation(self.gc1(features, edges))
         
         for i, l in enumerate(self.gc_module):
-            x = self.gc_module_1[i](x, edges)
-            x = self.activation(x)
+            x = self.activation(self.gc_module[i](x, edges))
         
-        x = residual1_1 + x
+        auxiliary = x
         
-        vertices = self.activation(self.gc1_14(x, edges))
+        x = residual + x
         
-        return vertices
+        vertices = self.activation(self.gc14(x, edges))
+        
+        return vertices, auxiliary
