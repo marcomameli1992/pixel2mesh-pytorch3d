78c6b58b9d299b7b30ea40b96a68f4496e19984c
Marco Mameli <mameli.1.marco@gmail.com>
2021-12-05 21:07:34 +0100

    Update to the shapened dataset
    This class manage the dataset but only open the images from the dataset and return the image path that in the train code can be used for the obj opening

--- data/dataloader/shapenet.py
+++ data/dataloader/shapenet.py
@@ -4,8 +4,14 @@ Dataset Management for the shapenet dataset
 :return: Dataset type
 :rtype: torch.dataset.Dataset
 """
+from typing import Tuple
+
 from torch.utils.data import Dataset
-from glob import glob
+from torchvision import transforms
+from pytorch3d.structures.meshes import join_meshes_as_batch
+from pytorch3d.io import load_objs_as_meshes, load_obj
+from skimage.io import imread
+import os
 
 class ShapeNetDataset(Dataset):
     """
@@ -14,14 +20,45 @@ class ShapeNetDataset(Dataset):
 
     """
 
-    def __init__(self, dataset_path: str):
+    def __init__(self, dataset_file_path: str, img_dataset_path: str, model_dataset_path: str, img_name: str, split: str, load_textures: bool = False):
         #% Init the dataset with the generation of the file list and object list
-        self.image_list = glob(dataset_path)
-        self.obj_list = glob(dataset_path)
 
+        self.transform = transforms.Compose([transforms.ToTensor()])
 
-    def __getitem__(self, item):
-        return item
+        with open(dataset_file_path + split + '.txt', 'r') as d_t:
+            self.img_list = d_t.readlines()
 
-    def __len__(self):
-        return 126
+        self.obj_list = []
+        delete = []
+
+        # Usare per aprire le mesh per la seconda parte
+        for i, s in enumerate(self.img_list):
+            if os.path.exists(s.replace(img_dataset_path, model_dataset_path).replace('rendering', 'models').replace(img_name + '.png', 'model_normalized.obj').strip()):
+                self.obj_list.append(s.replace(img_dataset_path, model_dataset_path).replace('rendering', 'models').replace(img_name + '.png', 'model_normalized.obj'))
+            else:
+                delete.append(s)
+
+        self.img_list = [f for f in self.img_list if f not in delete]
+        #self.obj_list = [s.replace(image_dataset_path, model_dataset_path).replace('rendering', 'models').replace(img_name + '.png', 'model_normalized.obj') for s in self.image_list]
+
+    def __getitem__(self, item) ->Tuple:
+        #% Get the item from dataset: Image and Obj
+
+        image = imread(self.img_list[item].strip())
+        image = self.transform(image)
+
+        data = {}
+        data['image'] = image
+        data['img_path'] = self.img_list[item].strip()
+
+        return data
+
+    def __len__(self) -> int:
+        return len(self.img_list)
+
+if __name__ == "__main__":
+    from torch.utils.data import DataLoader
+    dataset = ShapeNetDataset(dataset_file_path='/mnt/e/Progetti/ComputerGraphics/LandscapeGeneration-Vienna/MeshNetwork/pixel2mesh/data/dataloader/data_list/', img_dataset_path="/mnt/e/Progetti/ComputerGraphics/LandscapeGeneration-Vienna/MeshNetwork/Dataset/ShapeNet/ShapeNetP2M", model_dataset_path="/mnt/e/Progetti/ComputerGraphics/LandscapeGeneration-Vienna/MeshNetwork/Dataset/ShapeNet/ShapeNetCorev2", img_name='00', split='test')
+    dataloader = DataLoader(dataset=dataset, batch_size=2)
+    img = next(iter(dataloader))
+    print(img['image'].shape,img['img_path']) # the dictionary has the image element has tensor of image tensor and the img_path as list of string
