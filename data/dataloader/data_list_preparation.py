fba8015cdb03a0de15cf1671c7722ba57da7560a
Marco Mameli <mameli.1.marco@gmail.com>
2021-12-06 12:05:45 +0100

    Insert the classes for the possible pre-training configuration

--- data/dataloader/data_list_preparation.py
+++ data/dataloader/data_list_preparation.py
@@ -8,6 +8,22 @@ parser.add_argument('--configuration_file', '-cf', required=True, help="Represen
 
 args = parser.parse_args()
 
+classes = {
+    '02691156': 0,
+    '02828884': 1,
+    '02933112': 2,
+    '02958343': 3,
+    '03001627': 4,
+    '03211117': 5,
+    '03636649': 6,
+    '03691459': 7,
+    '04090263': 8,
+    '04256520': 9,
+    '04379243': 10,
+    '04401088': 11,
+    '04530566': 12
+}
+
 if __name__ == "__main__":
     with open(args.configuration_file, 'r') as config_file:
         config_parameter = json.load(config_file)
@@ -18,12 +34,12 @@ if __name__ == "__main__":
 
     with open(config_parameter['save_folder'] + 'train.txt', 'w') as train_file:
         for t in traindata:
-            train_file.write(t + '\n')
+            train_file.write(t + ',' + str(classes[t.replace(config_parameter['start_folder'], '').split('/')[0]]) + '\n')
 
     with open(config_parameter['save_folder'] + 'validation.txt', 'w') as val_file:
         for t in valdata:
-            val_file.write(t + '\n')
+            val_file.write(t + ',' + str(classes[t.replace(config_parameter['start_folder'], '').split('/')[0]]) + '\n')
 
     with open(config_parameter['save_folder'] + 'test.txt',  'w') as test_file:
         for t in testdata:
-            test_file.write(t + '\n')
+            test_file.write(t + ',' + str(classes[t.replace(config_parameter['start_folder'], '').split('/')[0]]) + '\n')
