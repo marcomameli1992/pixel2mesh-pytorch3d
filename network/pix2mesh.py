a83a2f08a616a8d1a5b2a47cfbb6ed0a2a0247fa
Marco Mameli <mameli.1.marco@gmail.com>
2021-12-07 19:43:11 +0100

    Update
    This file is not used for the moment

--- network/pix2mesh.py
+++ network/pix2mesh.py
@@ -10,11 +10,14 @@ class Pix2Mesh(Module):
         
         self.graph_convolutional_network = GraphConvolutional(first_block_input_dim=graph_convolutional_first_block_input_channels, second_block_input_dim=graph_convolutional_second_block_input_channels, third_block_input_dim=graph_convolutional_third_block_input_channels, hidden_dim=graph_convolutional_hidden_dim, output_dim=graph_convolutional_output_dim, hidden_layer_count=graph_convolutional_hidden_layer_count)
     
-    def forward(self, image, image_label, mesh):
+    def forward(self, image, mesh):
         conv16, conv32, conv64, conv128, conv256, conv512 = self.convolutional_network(image)
         
+        """if len(conv16.shape()) == 4 or len(conv32.shape()) == 4 or len(conv64.shape()) == 4 or len(conv128.shape()) == 4 or len(conv256.shape()) == 4 or len(conv512.shape()) == 4:
+            # Batch size different from 1
+            for i in conv64.shape()[0]:"""
         mesh1, mesh2, mesh3 = self.graph_convolutional_network(mesh, conv64, conv128, conv256)
-        
+                
         return mesh1, mesh2, mesh3
         
         
